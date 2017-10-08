7d7c028ab9ac026b9e919554e9602030279281cc
Vadzim <v@vadzim.info>
2017-10-08 18:24:07 +0300

    typing

--- source/define-constants.js
+++ source/define-constants.js
@@ -1,4 +1,6 @@
-export default function defineConstants(namespace = "", delimiter = ".") {
-	const prefix = namespace && `${namespace}${delimiter}`
+// @flow
+
+export default function defineConstants(namespace?: string = "", delimiter?: string = "."): { [string]: string } {
+	const prefix = namespace && namespace + delimiter
 	return new Proxy({}, { get: (_, name) => prefix + name })
 }
